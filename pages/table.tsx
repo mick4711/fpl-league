bf1778b5dea9162ebb844a08c89b908d5122216b
mick4711 <mick4711@gmail.com>
2021-01-27 17:48:07 +0000

    Add react table (#3)
    * add tables page with links and link styling
    
    * add @silevis/reactgrid with sample data
    
    * fix indentation

--- pages/table.tsx
+++ pages/table.tsx
@@ -5,54 +5,54 @@ import { ReactGrid, Column, Row, HeaderCell } from '@silevis/reactgrid'
 import '@silevis/reactgrid/styles.css'
 
 export default function Table() {
-    interface Person {
-        name: string;
-        surname: string;
-    }
+  interface Person {
+    name: string;
+    surname: string;
+  }
 
-    const getPeople = (): Person[] => [
-        { name: "Thomas", surname: "Goldman" },
-        { name: "Susie", surname: "Quattro" },
-        { name: "", surname: "" }
+  const getPeople = (): Person[] => [
+    { name: "Thomas", surname: "Goldman" },
+    { name: "Susie", surname: "Quattro" },
+    { name: "", surname: "" }
+  ]
+
+  const getColumns = (): Column[] => [
+    { columnId: "name", width: 150 },
+    { columnId: "surname", width: 150 }
+  ]
+
+  const headerRow: Row<HeaderCell> = {
+    rowId: "header",
+    cells: [
+      { type: "header", text: "Name" },
+      { type: "header", text: "Surname" }
     ]
-    
-    const getColumns = (): Column[] => [
-        { columnId: "name", width: 150 },
-        { columnId: "surname", width: 150 }
-    ]
+  }
 
-    const headerRow: Row<HeaderCell> = {
-        rowId: "header",
-        cells: [
-          { type: "header", text: "Name" },
-          { type: "header", text: "Surname" }
-        ]
-    }
+  const getRows = (persons: Person[]): Row[] => [
+    headerRow,
+    ...persons.map<Row>((person, idx) => ({
+      rowId: idx,
+      cells: [
+        { type: "text", text: person.name },
+        { type: "text", text: person.surname }
+      ]
+    }))
+  ]
 
-    const getRows = (persons: Person[]): Row[] => [
-        headerRow,
-        ...persons.map<Row>((person, idx) => ({
-          rowId: idx,
-          cells: [
-            { type: "text", text: person.name },
-            { type: "text", text: person.surname }
-          ]
-        }))
-    ]
+  const [people] = React.useState<Person[]>(getPeople());
 
-    const [people] = React.useState<Person[]>(getPeople());
-  
-    const rows = getRows(people);
-    const columns = getColumns();
-  
-    return <div>
-        <Head>
-            <title>FPL Table</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+  const rows = getRows(people);
+  const columns = getColumns();
 
-        <Link href='/'>Home</Link>
+  return <div>
+    <Head>
+      <title>FPL Table</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-        <ReactGrid rows={rows} columns={columns} />
-    </div>
+    <Link href='/'>Home</Link>
+
+    <ReactGrid rows={rows} columns={columns} />
+  </div>
 }
