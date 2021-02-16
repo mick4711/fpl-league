5a98521c69536376fe7f85b386166d327b1cd456
mick4711 <mick4711@gmail.com>
2021-02-16 18:56:49 +0000

    Add links (#9)
    * tried adding hyperlinks
    
    * add 2nd table with links
    
    * remove redundant info
    
    * use basic table as a component

--- pages/table.tsx
+++ pages/table.tsx
@@ -1,62 +1,49 @@
 import { GetStaticProps } from "next"
 import Head from "next/head"
 import Link from "next/link"
-import { ReactGrid, Column, Row, HeaderCell } from "@silevis/reactgrid"
-import "@silevis/reactgrid/styles.css"
-
-interface Player {
-  id: number
-  gw: number
-  name: string
-  team: string
-  points: number
-  rank: number
-  gw_points: number
-  gw_rank: number
-  link: string
-}
+// import { ReactGrid, Column, Row, HeaderCell } from "@silevis/reactgrid"
+// import "@silevis/reactgrid/styles.css"
+import LeagueTable, { Player } from "../components/league-table"
 
 export default function Table({ playerData }: { playerData: Player[] }) {
-  const getColumns = (): Column[] => [
-    { columnId: "name", width: 150 },
-    { columnId: "team", width: 150 },
-    { columnId: "points", width: 60 },
-    { columnId: "rank", width: 100 },
-    { columnId: "gw_points", width: 60 },
-    { columnId: "gw_rank", width: 100 },
-  ]
+  // const getColumns = (): Column[] => [
+  //   { columnId: "name", width: 150 },
+  //   { columnId: "team", width: 150 },
+  //   { columnId: "points", width: 60 },
+  //   { columnId: "rank", width: 100 },
+  //   { columnId: "gw_points", width: 60 },
+  //   { columnId: "gw_rank", width: 100 },
+  // ]
 
-  const headerRow: Row<HeaderCell> = {
-    rowId: "header",
-    cells: [
-      { type: "header", text: "Name" },
-      { type: "header", text: "Team" },
-      { type: "header", text: "Points" },
-      { type: "header", text: "Rank" },
-      { type: "header", text: "GW-Pts" },
-      { type: "header", text: "GW-Rank" },
-    ],
-  }
+  // const headerRow: Row<HeaderCell> = {
+  //   rowId: "header",
+  //   cells: [
+  //     { type: "header", text: "Name" },
+  //     { type: "header", text: "Team" },
+  //     { type: "header", text: "Points" },
+  //     { type: "header", text: "Rank" },
+  //     { type: "header", text: "GW-Pts" },
+  //     { type: "header", text: "GW-Rank" },
+  //   ],
+  // }
 
-  const getRows = (players: Player[]): Row[] => [
-    headerRow,
-    ...players.map<Row>((person, idx) => ({
-      rowId: idx,
-      cells: [
-        { type: "text", text: person.name },
-        { type: "text", text: person.team },
-        { type: "number", value: person.points },
-        { type: "number", value: person.rank },
-        { type: "number", value: person.gw_points },
-        { type: "number", value: person.gw_rank },
-      ],
-    })),
-  ]
+  // const getRows = (players: Player[]): Row[] => [
+  //   headerRow,
+  //   ...players.map<Row>((person, idx) => ({
+  //     rowId: idx,
+  //     cells: [
+  //       { type: "text", text: person.name },
+  //       { type: "text", text: person.team },
+  //       { type: "number", value: person.points },
+  //       { type: "number", value: person.rank },
+  //       { type: "number", value: person.gw_points },
+  //       { type: "number", value: person.gw_rank },
+  //     ],
+  //   })),
+  // ]
 
-  // const getMyRows = (players: Player[]): [] => []
-
-  const rows = getRows(playerData)
-  const columns = getColumns()
+  // const rows = getRows(playerData)
+  // const columns = getColumns()
   const gameweek = playerData[0]["gw"]
   const timestamp = new Date()
 
@@ -81,30 +68,9 @@ export default function Table({ playerData }: { playerData: Player[] }) {
             minute: "numeric",
           })}
         </p>
-        <ReactGrid rows={rows} columns={columns} />
-        <hr />
-        <table>
-          <tr>
-            <td>Name</td>
-            <td>Team</td>
-            <td>Points</td>
-            <td>Rank</td>
-            <td>GW Points</td>
-            <td>GW Rank</td>
-          </tr>
-          {playerData.map((player) => (
-            <tr>
-              <td>{player.name}</td>
-              <td>
-                <a href={player.link}>{player.team}</a>
-              </td>
-              <td>{player.points}</td>
-              <td>{player.rank}</td>
-              <td>{player.gw_points}</td>
-              <td>{player.gw_rank}</td>
-            </tr>
-          ))}
-        </table>
+        {/* <ReactGrid rows={rows} columns={columns} />
+        <hr /> */}
+        <LeagueTable playerData={playerData} />
       </div>
     </div>
   )
