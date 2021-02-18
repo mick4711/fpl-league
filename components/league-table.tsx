6acc0616334cae3f6e3a889f8bf693e72f8fb654
mick4711 <mick4711@gmail.com>
2021-02-18 11:31:47 +0000

    Add sorting (#10)
    * fix table format with <thead>
    
    * use State to sort
    
    * hover feedback and avoid unnecessary default sort

--- components/league-table.tsx
+++ components/league-table.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 export interface Player {
   id: number
   gw: number
@@ -10,23 +12,64 @@ export interface Player {
   link: string
 }
 
+enum Column {
+  Points,
+  GW_Points,
+}
+
 export default function LeagueTable({ playerData }: { playerData: Player[] }) {
+  const [column, setColumn] = useState(Column.Points)
+  const [doSort, setDoSort] = useState(false)
+
+  const sortTable = () => {
+    switch (column) {
+      case Column.Points:
+        playerData.sort((a, b) => {
+          return b.points - a.points
+        })
+        break
+      case Column.GW_Points:
+        playerData.sort((a, b) => {
+          return b.gw_points - a.gw_points
+        })
+        break
+    }
+  }
+
+  if (doSort) {
+    sortTable()
+  }
+
   return (
     <table>
-      <tr className="shaded">
-        <td>Name</td>
-        <td>Team</td>
-        <td>Points</td>
-        <td>Rank</td>
-        <td>GW Points</td>
-        <td>GW Rank</td>
-      </tr>
+      <thead>
+        <tr className="shaded">
+          <td>Name</td>
+          <td>Team</td>
+          <td className="sort" onClick={() => {
+            setColumn(Column.Points)
+            setDoSort(true)
+          }}>
+            Points
+          </td>
+          <td>Rank</td>
+          <td className="sort" onClick={() => {
+            setColumn(Column.GW_Points)
+            setDoSort(true)
+          }}>
+            GW Points
+          </td>
+          <td>GW Rank</td>
+        </tr>
+      </thead>
       <tbody>
         {playerData.map((player) => (
           <tr key={player.id}>
             <td>{player.name}</td>
             <td>
-              <a href={player.link}>{player.team}</a>
+              <a href={player.link} target="_blank" rel="noreferrer">
+                {player.team}
+              </a>
             </td>
             <td className="numeric">{player.points.toLocaleString()}</td>
             <td className="numeric">{player.rank.toLocaleString()}</td>
