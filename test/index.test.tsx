0112da75a0357c5cec941b7633a368794fef4d97
mick4711 <mick4711@gmail.com>
2021-02-26 19:35:57 +0000

    added snapshot

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,7 +1,17 @@
 import { render } from "@testing-library/react"
+import renderer from "react-test-renderer"
 import Table from "../pages/index"
 import { validPlayerData } from "./mock-data"
 
+
+beforeAll(() => {
+  jest.useFakeTimers('modern')
+  jest.setSystemTime(new Date('09 Apr 1957 19:11:00 GMT').getTime())
+})
+
+afterAll(() => {
+  jest.useRealTimers()
+})
 
 describe("Table component", () => {
   it("renders with valid playerData", () => {
@@ -20,9 +30,7 @@ describe("Table component", () => {
   })
 
   it("renders with empty playerData and message", () => {
-    const { getByRole, getByText } = render(
-      <Table playerData={[]} />
-    )
+    const { getByRole, getByText } = render(<Table playerData={[]} />)
 
     const message = getByText("Gameweek: No data returned from server")
     const title = getByText("Fantasy Football customisable tables")
@@ -34,5 +42,12 @@ describe("Table component", () => {
     expect(main).toBeInTheDocument()
     expect(table).toBeInTheDocument()
     expect(table.className).toEqual(expect.stringContaining("leaguetable"))
+  })
+
+
+  it("renders snapshot correctly", () => {
+    const component = renderer.create(<Table playerData={validPlayerData} />)
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
   })
 })
