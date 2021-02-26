50f2a2078f2cf64481c132ad50e4ea17865fce91
mick4711 <mick4711@gmail.com>
2021-02-26 17:37:06 +0000

    add table and row to container

--- test/td-sort.test.tsx
+++ test/td-sort.test.tsx
@@ -4,6 +4,15 @@ import TdSort from "../components/td-sort"
 const colName = "column name"
 const sortByColumn = (columnName: string) => {}
 
+let containerWithTableRow: { container: HTMLTableRowElement }
+beforeEach(() => {
+  const table = document.createElement("table")
+  const tr = document.createElement("tr")
+  containerWithTableRow = {
+    container: document.body.appendChild(table).appendChild(tr),
+  }
+})
+
 describe("TdSort component", () => {
   it("renders with correct class when sorted", () => {
     const { getByRole, getByAltText } = render(
@@ -11,7 +20,8 @@ describe("TdSort component", () => {
         title={colName}
         onColumnClick={sortByColumn}
         sortedStyle={true}
-      />
+      />,
+      containerWithTableRow
     )
     const td = getByRole("cell", { name: colName })
     const arrow = getByAltText("down arrow")
@@ -28,7 +38,8 @@ describe("TdSort component", () => {
         title={colName}
         onColumnClick={sortByColumn}
         sortedStyle={false}
-      />
+      />,
+      containerWithTableRow
     )
     const td = getByRole("cell", { name: colName })
     const arrow = getByAltText("down arrow")
